f65b916b0c46aade33a40238cc7201e0e461ad1e
Anna Fedrunova <a.fedrunova@gmail.com>
2019-08-06 13:49:56 +0300

    Delete servise files

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
-import './style.css'
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#mount-point')
-);
